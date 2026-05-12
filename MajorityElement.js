57338f49254952b7e9e2463c57e94cb03ea34406
sonu_yadav_16 <sonu.yadav@primafelicitas.com>
2026-05-12 11:49:55 +0530

    Solve Majority Element problem

---
+++ MajorityElement.js
@@ -0,0 +1,16 @@
+//Leetcode 169. Majority Element
+
+var majorityElement = function(nums) {
+    let result = nums[0]
+    let count = 0;
+    for(let i=0; i<nums.length; i++) {
+        if(count === 0) result = nums[i]
+
+        if(nums[i] === result) count++;
+        else count--;
+    }
+    return result;
+};
+
+console.log(majorityElement([3,2,3]))
+console.log(majorityElement([2,2,1,1,1,2,2]))
